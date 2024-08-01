65c0c9e891651001e2505754edae540a818c9fe4
VladimirSamaell <samaell19945@gmail.com>
2024-08-01 17:16:01 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1, True, 'String') #immutable_var - не изменяемый тип данных!
+print(immutable_var)
+#immutable_var[1] = 3
+#print(immutable_var)
+mutable_list = [1, 1, 2, 3]
+print(mutable_list)
+mutable_list[0] = 0
+print(mutable_list)
