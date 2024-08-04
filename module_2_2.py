b17ab97cbfde7136efbfb367ce2f856f84f047f5
VladimirSamaell <samaell19945@gmail.com>
2024-08-04 22:53:13 +0300

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -2,9 +2,9 @@ number = input('Введите 3 числа:').split()
 first = number[0]
 second = number[1]
 third = number[2]
-if first == second and first == third:
+if first == second == third:
     print(3)
 elif first == second or first == third or second == third or second == first:
     print(2)
-elif first != second and first != third:
+elif second != first != third:
     print(0)
